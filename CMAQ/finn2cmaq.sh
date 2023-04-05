bf10b7d73c043e216816d4167f64ac5558f1223d
ramespada <espada@agro.uba.ar>
2023-04-05 19:59:49 -0300

    finn2 cmaq avances, pero anda lento!

--- CMAQ/finn2cmaq.sh
+++ CMAQ/finn2cmaq.sh
@@ -14,19 +14,17 @@ srsOut="+proj=lcc +lat_1=${truelat1} +lat_2=${truelat2} +lon_0=${stand_lon} +lat
 read xc yc ellipsoidh <<<$( gdaltransform -s_srs "epsg:4326" -t_srs "${srsOut}" <<< $( echo "${ref_lon} ${ref_lat}" ) )
 
 #Grilla:
-nx=197;ny=237;dx=20000;dy=20000;                #ncols,nrows,xcel, ycell
+nx=197;ny=237;nz=1;dx=20000;dy=20000;                #ncols,nrows,xcel, ycell
 xorig=-1970000; yorig=-2370000 ;
 xmin=$(bc -l <<<" $xc - ($xorig)*-1 ")
 xmax=$(bc -l <<<" $xc + ($xorig)*-1 ")
 ymin=$(bc -l <<<" $yc - ($yorig)*-1 ")
 ymax=$(bc -l <<<" $yc + ($yorig)*-1 ")
 
-
 #Creo carpetas:
 mkdir finn_data		#carpeta de datos descargados de FINN.
 
 #---------------------------------------
-#Get FINN data:
   thisYear=$( date +"%Y")
 
 start_date_s=$(date -d "$start_date" +%s)
@@ -40,6 +38,7 @@ do
 	 DDD=$(date -d @$day +"%j")   #dia juliano
 	echo "Dia: $YYYY-$DDD (mes: $MM)"
 
+	#Get FINN data:
 	 if [ ! -f finn_data/GLOB_${chemistry}_$YYYY$DDD.txt ]
 	 then
 	 	if [ $YYYY -lt $(($thisYear-2)) ]	#los años viejos los tienen en carpetas.
@@ -51,16 +50,20 @@ do
 		
 		gzip -d finn_data/GLOB_${chemistry}_$YYYY$DDD.txt.gz
 	fi;
-
 	finnFile=finn_data/GLOB_${chemistry}_$YYYY$DDD.txt
-	#Obtengo lista de polluts a inventariar
+	
+	#Del archivo descargado obtengo lista de polluts a inventariar
 	polluts=($(head -n1 $finnFile | sed 's/,/ /g;s/^.*AREA //g' ))
-
         swap_list=$(printf "%-16s" ${polluts[@]})
         var_list="$swap_list"
 
+	#Transformo coordenadas + me quedo con los puntos dentro del dominio.
+	cat $finnFile | sed 's/D\([-+]\)/e\1/g;s/,/;/g' | awk -F";" 'NR==1{print $0";wkt"} NR>=2{print $0"POINT("$5*1.0,$4*1.0")"}' > tmp.csv
+	ogr2ogr -f CSV tmp_xy.csv tmp.csv -s_srs "$srsInp" -t_srs "$srsOut" -clipsrc $xmin $ymin $xmax $ymax -lco GEOMETRY="AS_WKT" -lco SEPARATOR="SEMICOLON"
+	sed -i 's/"//g;s/  */ /g;' tmp_xy.csv
+
 	#Crear template de NetCDF 
-cat  > netcdf_emission_template.cdl <<EOF
+	cat  > netcdf_emission_template.cdl <<EOF
 netcdf emissionInventory {
 dimensions:
     TSTEP = 24;
@@ -74,27 +77,21 @@ variables:
     int TFLAG(TSTEP, VAR, DATE_TIME) ;
     float Band1(ROW, COL);
     //float emission(time, y, x);
-    //Mis variables:
-
+EOF
+printf "     float %s(TSTEP, LAY, ROW, COL);\n" ${polluts[@]} >> netcdf_emission_template.cdl #   //Mis variables:
+cat >> netcdf_emission_template.cdl << EOF
 // global attributes:
  :IOAPI_VERSION = "ioapi-3.2: \$Id: init3" ; :EXEC_ID = "???????????????? " ; 
- :FTYPE = 1 ;
- :SDATE = $YYYY$DDD ; :STIME = 000000 ; :WDATE = 2020001 ; :WTIME = 000000 ; :CDATE = 2020000 ; :CTIME = 000000;
- :TSTEP = 10000      ; :NTHIK = 1         ;    
- :NCOLS = $nx        ;  :NROWS = $ny      ;  :NLAYS = $nz    ; :NVARS = ${#polluts[@]} ; :GDTYP = 2 ;
- :P_ALP = -50. ; :P_BET = -20. ; :P_GAM = -65. ; 
- :XCENT = $clon ; :YCENT = $clat ; :XORIG = $xorig ; :YORIG = $yorig ; :XCELL = $dx ;  :YCELL = $dy ;  
- :VGTYP = -9999 ;        :VGTOP = 5000.f ;       :VGLVLS = 1.f, 0.9938147f ;    
- :GDNAM = "$GridName" ;  :UPNAM = "OUTCM3IO        " ;   :VAR-LIST = "${var_list}";      
+ :FTYPE = 1 ; :SDATE = $YYYY$DDD ; :STIME = 000000 ; :WDATE = 2020001 ; :WTIME = 000000 ; :CDATE = 2020000 ; :CTIME = 000000;
+ :TSTEP = 10000; :NTHIK = 1;    
+ :NCOLS = $nx; :NROWS = $ny;  :NLAYS = $nz; :NVARS = ${#polluts[@]}; :GDTYP = 2;
+ :P_ALP = -50.;:P_BET = -20. ;:P_GAM = -65.; 
+ :XCENT = $ref_lon; :YCENT = $ref_lat; :XORIG = $xorig; :YORIG = $yorig; :XCELL = $dx;  :YCELL = $dy;  
+ :VGTYP = -9999 ;:VGTOP = 5000.f ;:VGLVLS = 1.f, 0.9938147f;    
+ :GDNAM = "$GridName";:UPNAM = "OUTCM3IO" ;   :VAR-LIST = "${var_list}";      
  :FILEDESC = "Merged emissions output file from Mrggrid" ; :HISTORY = "" ;
 }
 EOF
-#Agrego las variables a inventariar:
-for pollut in ${polluts[@]}
-do
-                sed -i "/\/\/Mis variables:/a     float $pollut(TSTEP, LAY, ROW, COL) ;" netcdf_emission_template.cdl
-done;
-
 
 file_out=fire_emis_${YYYY}${DDD}_d01.nc
 ncgen -o $file_out netcdf_emission_template.cdl
@@ -103,47 +100,47 @@ hours=(00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
 for HH in ${hours[@]}
 do
 	echo "   HORA: $HH"
-	i=$(( $HH % 24 ));                      #indice de variable time
+	i=$(echo $HH | awk '{print $0*1}');
 
-for j in ${!polluts[@]}
-do
-	pollut=${polluts[$j]}
-	echo "      pollut: $pollut"
-#---------------------------------------
-#Parse FINN files:
-
-#Filtro columnas lat,lon y pollut, y solo las filas que el date caiga dentro de la hora. tambien convierto emis a mole/s ó g/s.
-cat $finnFile | sed 's/D\([-+]\)/e\1/g' | awk -F"," -v pollut=${pollut} -v hour=${HH}00 '
-NR==1{
+	awk -F";" -v hour="${HH}00" '
+	NR==1{OFS=";"; print$0; for(i=1;i<=NF;i++){if($i=="TIME"){time=i};}; h_ini=hour; h_fin=hour+60;} 
+	NR >1{ if( $time >= h_ini                  ){print $0};  }' tmp_xy.csv > tmp_HH.csv
+	#NR>1{ if( $time >= h_ini && $time < h_fin ){print $0};  }' tmp_xy.csv > tmp_HH.csv
+	
+	
+	for j in ${!polluts[@]}
+	do
+		pollut=${polluts[$j]}
+		echo "      pollut: $pollut"
+	#---------------------------------------
+	#Filtro columnas wkt y pollut, tambien convierto emis a mole/s ó g/s.
+	awk -F";" -v pollut=${pollut} '
+	NR==1{
 	for(i=1;i<=NF;i++){if($i==pollut){pol=i};};
-	print "wkt;emis";
-
-	if ( pollut == "OC" || pollut =="BC" || pollut=="PM25" || pollut == "PM10" ){
-		k=1/86400000.0;		#kg/day -> g/s	
-	}else {
-		k=1/86400.0;		#mole/day -> mole/s
-	};
-} 
-NR>1{
-if($2>=hour && $2<(hour+60))  {
-	printf("POINT(%.4f %.5f); %.5e\n", $5,$4, $pol/k);
-};
-}'>tmp.csv
-
-ogr2ogr -s_srs "$srsInp" -t_srs "$srsOut" -f CSV tmp_xy.csv tmp.csv -lco GEOMETRY=AS_WKT -lco SEPARATOR="SEMICOLON"
-
-#---------------------------------------
-#ASCII to netcdf:
-
-#gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" -ot Float32 tmp_xy.csv swap.nc -co WRITE_LONLAT="YES"  -co WRITE_BOTTOMUP="NO"
-gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of GTiff tmp_xy.csv tmp.tif
-#Transformar a NetCDF
-gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" tmp.tif swap.nc -co "WRITE_LONLAT=YES" -co "WRITE_BOTTOMUP=NO"
-
-ncks  -A -V -v Band1 swap.nc -o $file_out
-ncap2 -A -s "${pollut}($i,0,:,:) = Band1(:,:); TFLAG($i,$j,0) = ${YYYY}${DDD}; TFLAG($i,$j,1) =  ${HH}0000;" $file_out $file_out
-
+		print "wkt;emis";
+		if ( pollut == "OC" || pollut =="BC" || pollut=="PM25" || pollut == "PM10" ){
+			k=1/86400000.0;		#kg/day -> g/s	
+		}else {
+			k=1/86400.0;		#mole/day -> mole/s
+		};
+	} 
+	NR>1{ printf("%s;%.5e\n", $1, $pol*k); }' tmp_HH.csv > tmp_HH_pollut.csv
+	
+	#---------------------------------------
+	##ASCII to netcdf
+	#gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" -ot Float32 tmp_xy.csv swap.nc -co WRITE_LONLAT="YES"  -co WRITE_BOTTOMUP="NO"
+	gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of GTiff tmp_HH_pollut.csv tmp.tif
+	##Transformar a NetCDF
+	gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" tmp.tif swap.nc #-co "WRITE_LONLAT=YES" -co "WRITE_BOTTOMUP=NO"
+	# (estos son los que hacen que tarde mucho):
+	ncbo -p 3 ncks  -A -V -v Band1 swap.nc -o $file_out
+	ncbo -p 3 ncap2 -A -s "${pollut}($i,0,:,:) = Band1(:,:); TFLAG($i,$j,0) = ${YYYY}${DDD}; TFLAG($i,$j,1) =  ${HH}0000;" $file_out 
+	
+	done;
+	
 done;
-done;
+
+
 	day=$(($day + 86400 )) # Incrementar day un dia
 done;
+
