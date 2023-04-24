d53dd15533ddf0d5563f4dbd2fa29b9b310ae97c
ramespada <espada@agro.uba.ar>
2023-04-24 18:33:03 -0300

    regrid datos

--- CMAQ/megan2cmaq/prepmegan4cmaq.f90
+++ CMAQ/megan2cmaq/prepmegan4cmaq.f90
@@ -31,13 +31,12 @@ program prepmegan4cmaq
   logical :: file_exists
 
 
-  namelist /control/start_date,end_date,finn_data_directory,griddesc_file,diurnal_cycle
 
 
 
 
-
-
+  namelist /control/start_date,end_date,griddesc_file
+  namelist /inp_files/crop_frac_file,grass_frac_file,shrub_frac_file,tree_frac_file, nl_tree_frac_file, bl_tree_frac_file, tp_tree_frac_file,ecotype_file,lai_file,veg_cov_file,wrfout_file
 
   call date_and_time(values=todays_date)       !fecha de hoy.
 
@@ -45,7 +44,7 @@ program prepmegan4cmaq
   !open(7, file='parameters'); read(7,parameters); close(7) !leo namelist
   read(*,nml=control, iostat=iostat)
   if( iostat /= 0 ) then
-    write(*,*) 'finn2cmaq: failed to read namelist; error = ',iostat
+    write(*,*) 'prepmegan4cmaq: failed to read namelist; error = ',iostat
     stop
   end if
 
@@ -86,8 +85,6 @@ program prepmegan4cmaq
 
     current_date_s=current_date_s + 86400  !siguiente día!
   end do
-
-
 
 
 print*, "========================================="
@@ -138,4 +135,89 @@ contains
     rtoa = adjustl(rtoa)
  end function
 
+ subroutine read_GRIDDESC(griddescFile, p, g)
+        implicit none
+        character(256),intent(in) :: griddescFile
+        type(proj_type) ,intent(inout) :: p
+        type(grid_type) ,intent(inout) :: g
+        character(10) :: dummyvar
+        open(2,file=griddescFile, status='old', action='read')                                  !GRIDDESC:
+           read(2,*) dummyvar;                                                   !' '
+           read(2,*) p%pName;                                                    !projName
+           read(2,*) p%typ,p%truelat1,p%truelat2,p%stand_lon,p%ref_lon,p%ref_lat !map_proj truelat1 truelat2 stand_lon ref_lon ref_lat
+           read(2,*) dummyvar;                                                   !' '
+           read(2,*) g%gName;                                                    !gridName
+           read(2,*) p%pName,g%xmin,g%ymin,g%dx,g%dy,g%nx,g%ny                   !projName xorig yorig xcell ycell nrows ncols
+        close(2)
+
+        !Calcular otros parametros:
+        if (p%typ == 1 ) then           !Geographic:
+                p%typ_str='ll';   p%proj4="+proj=latlong +a=6370000.0 +b=6370000.0"
+        else if ( p%typ == 2 ) then     !Lambert Conformal Conic:
+                p%typ_str='lcc';  p%proj4="+proj=lcc +lat_1="//trim(rtoa(p%truelat1))//" +lat_2="//trim(rtoa(p%truelat2))//" +lon_0="//trim(rtoa(p%stand_lon))//" +lat_0="//trim(rtoa(p%ref_lat))//" +a=6370000.0 +b=6370000.0 +units=m"
+        else if ( p%typ == 3 ) then     !General Mercator
+                p%typ_str="merc"; p%proj4="+proj=merc +lat_ts=${truelat1} +a=6370000.0 +b=6370000.0"
+        else if ( p%typ == 4 ) then     !General tangent Stereografic
+                p%typ_str='stere';p%proj4="+proj=stere +lat_0=${ref_lat} +lon_0=${stand_lon} +lat_ts=lat_ts +a=6370000.0 +b=6370000.0 +k_0=1.0"
+        else if ( p%typ == 5 ) then     !UTM
+                p%typ_str='utm';  p%proj4="+proj=utm +zone="
+        else if ( p%typ == 6 ) then     !Polar Secant Stereographic
+                p%typ_str='stere';p%proj4="+proj=stere +lat_0=${ref_lat} +lon_0=${stand_lon} +lat_ts=lat_ts +a=6370000.0 +b=6370000.0 +k_0=1.0"
+        else if ( p%typ == 7 ) then     !Equatorial Mercator
+                p%typ_str="merc"; p%proj4="+proj=merc +lat_ts=${truelat1} +a=6370000.0 +b=6370000.0"
+        else if ( p%typ == 8 ) then     !Transverse Mercator
+                p%typ_str='ll';   p%proj4="+proj=latlong +a=6370000.0 +b=6370000.0"
+        else if ( p%typ == 9 ) then     !Lambert Azimuthal Equal-Area
+                print*, "proyección: 9 (Lambert Azimuthal Equal-Area) no soportada por esta aplicación."; stop
+        else
+                print*, "codigo de proyección invalido.", p%typ; stop
+        end if
+
+        !Obtener coordenadas del centro de la grilla, min y max:
+        g%xc=0.0;g%yc=0.0; g%xmax=(g%xmin)*(-1); g%ymax=(g%ymin)*(-1)
+
+        !transformo boundaries a latlon
+        call gdalTransform(g%xmin,g%ymin,g%lonmin,g%latmin,p%proj4,'epsg:4326')
+        call gdalTransform(g%xmax,g%ymax,g%lonmax,g%latmax,p%proj4,'epsg:4326')
+
+ end subroutine
+
+ subroutine gdalTransform(x1,y1,x2,y2,srs1,srs2)
+        implicit none
+        real, intent(in)   :: x1,y1
+        real, intent(inout):: x2,y2
+        character(*)   :: srs1,srs2
+        character(10)  :: ellipsoidh
+        character(256) :: command
+        command="echo "//rtoa(x1)//" "//rtoa(y1)//" | gdaltransform -s_srs '"//trim(srs1)//"' -t_srs '"//trim(srs2)//"'  > tmp_gdal.txt";
+        call system(trim(command))
+        !print*,trim(command)
+        open(9, file='tmp_gdal.txt', status='old',action='read'); read(9,*, iostat=status) x2, y2, ellipsoidh;  close(9)
+        call system('rm tmp_gdal.txt')
+ end subroutine
+
+
 end program
+
+
+
+!ESTRUCTURA GENERAL de MEGAN_PREP_Code_Jan_2022:
+!(1) Leen Namelist
+!(2) call wrf_file         (leen wrfout para sacar algunos parametros de grilla y proyeccion)
+!(3) call  megan2_bioemiss (creo que aca es donde interpola)
+!(4) call write_*
+!       prepmegan4cmaq_arid.f90:         write(99,'(a)')"CID, ICELL, JCELL,  ARID"
+!       prepmegan4cmaq_cantype.f90:      write(99,'(a)')"CID,ICELL,JCELl,NEEDL,TROPI,BROAD,SHRUB,HERB,CROP"
+!       prepmegan4cmaq_ecotype.f90:      write(99,'(a)')"gridID,EcotypeID,EcotypeFrac"
+!       prepmegan4cmaq_ef.f90:           write(99,'(a)')"CELL_ID,X,Y,LAT,LONG,DSRAD,DTEMP,ISOP,MYRC,SABI,LIMO,A_3CAR,OCIM,BPIN,APIN,OMTP,FARN,BCAR,OSQT,MBO,MEOH,ACTO,CO,NO,BIDER,STRESS,OTHER"
+!       prepmegan4cmaq_fert.f90:         write(99,888)"CELL_ID,X,Y,LAT,LONG",fertday
+!       prepmegan4cmaq_grwform.bck.f90:  write(99,'(a)')"gridID,TreeFrac,CropFrac,ShrubFrac,HerbFrac"
+!       prepmegan4cmaq_grwform.f90:      write(99,'(a)')"gridID,TreeFrac,CropFrac,ShrubFrac,HerbFrac"
+!       prepmegan4cmaq_lai.f90:          write(99,'(a)')"CELL_ID,X,Y,LAT,LONG,LAI01,LAI02,LAI03,LAI04,LAI05,LAI06,LAI07,LAI08,LAI09,LAI10, &
+!       prepmegan4cmaq_landtype.f90:     write(99,'(a)')"CELL_ID,X,Y,LAT,LONG,LANDTYP"
+!       
+!       prepmegan4cmaq_nitrogen.f90:     write(99,'(a)')"CELL_ID,X,Y,LAT,LONG,NITROGEN01,NITROGEN02,NITROGEN03,NITROGEN04,NITROGEN05,NITROGEN06,NITROGEN07,NITROGEN08,NITROGEN09,NITROGEN10, &
+!       prepmegan4cmaq_non_arid.f90:     write(99,'(a)')"CID, ICELL, JCELL,  NON_ARID"
+!       prepmegan4cmaq_pft.f90:          write(99,'(a)')"CID, ICELL, JCELL,  NT_EG_TEMP,  NT_DC_BORL,  NT_EG_BORL, &
+!       prepmegan4cmaq_w126.f90:         write(99,'(a)')"CID, ICELL, JCELL,  W126(ppm-hours)"
+!(5) clean-up (deallocate)
