e85ec5713b59f54f2741b00669ba8dbba54f705a
ramespada <espada@agro.uba.ar>
2023-09-19 13:24:50 -0300

    ocean2cmaq casi terminado la version shell

--- CMAQ/emis/ocean4cmaq/input/build_data.sh
+++ CMAQ/emis/ocean4cmaq/input/build_data.sh
@@ -2,9 +2,9 @@
 export LC_NUMERIC="en_US.UTF-8"
 #------------------------------------------------
 #Input-Data:
-   landpoly=shp/land_10m_global.gpkg	                 #polygons to make a landmask
-  chlor_dir=/home/rama/data/ocean/MY1DMM_CHLORA_2019     #chlorophyl conc. files (from MODIS, MY1DMM)
-   dms_file=dms_lana_2011.nc                             #dms conc. files (DMS climatology of Lana et al. (2011))
+ landpoly=shp/land_10m_global.gpkg	                 #polygons to make a landmask
+chlor_dir=/home/rama/data/ocean/MY1DMM_CHLORA_2019     #chlorophyl conc. files (from MODIS, MY1DMM)
+ dms_file=dms_lana_2011.nc                             #dms conc. files (DMS climatology of Lana et al. (2011))
 #------------------------------------------------
 # CHLO
 #
@@ -13,20 +13,22 @@ export LC_NUMERIC="en_US.UTF-8"
 #
 if [ ! -f CHLO_monthly_global.nc ]
 then
+echo "bulinding CHLO file"
    for mm in $(seq --format="%02.0f" 1 12)
    do
-       m=${mm#0} #"$(printf "%d" ${mm})";
-       file=${chlor_dir}/MY1DMM_CHLORA_2019-${mm}-01_rgb_3600x1800.FLOAT.TIFF
+      m=${mm#0} 
+      file=${chlor_dir}/MY1DMM_CHLORA_2019-${mm}-01_rgb_3600x1800.FLOAT.TIFF
 
-       gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" ${file} tmp0.nc 
-       ncks -4 -A -v Band1 tmp0.nc tmp.nc
+      gdal_translate -q -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" ${file} tmp0.nc 
+      ncks -4 -A -v Band1 tmp0.nc tmp.nc
 
-       ncrename -v Band1,chlo${mm} tmp.nc
-       ncatted -h -O -a units,chlo${mm},o,c,"mg m^-3" tmp.nc
-       ncatted -h -O -a long_name,chlo${mm},o,c,"CHLO" tmp.nc
-       ncatted -h -O -a standard_name,chlo${mm},o,c,"mass_concentration_of_chlorophyll_in_sea_water" tmp.nc
-       ncatted -h -O -a var_desc,chlo${mm},o,c,"Chlorophyll Concentration, from MY1DMM MODIS product." tmp.nc
-       ncatted -h -O -a missing_value,chlo${mm},o,s,"99999" tmp.nc
+      ncrename -v Band1,chlo${mm} tmp.nc
+      ncatted -h -O -a units,chlo${mm},o,c,"mg m^-3" tmp.nc
+      ncatted -h -O -a long_name,chlo${mm},o,c,"CHLO" tmp.nc
+      ncatted -h -O -a standard_name,chlo${mm},o,c,"mass_concentration_of_chlorophyll_in_sea_water" tmp.nc
+      ncatted -h -O -a var_desc,chlo${mm},o,c,"Chlorophyll Concentration, from MY1DMM MODIS product." tmp.nc
+      ncap2 -h -A -v -s "where( chlo${mm} > 9999 ) chlo${mm}=0" tmp.nc tmp.nc
+      ncatted -h -O -a missing_value,chlo${mm},o,f,"99999" tmp.nc
    done
    ncatted -h -O -a Source_Software,global,d,c,"" tmp.nc
    ncatted -h -O -a Conventions,global,d,c,"" tmp.nc
@@ -43,25 +45,27 @@ fi
 # DMS
 #  I already have this data from SILAM dataset (dms_lana_2011.nc).
 #  DMS is in M and i should convert it to nM
-if [ ! -f DMS_monthly_global.nc ]
+if [ ! -f DMS_monthly_global_test.nc ]
 then
+echo "bulinding DMS file"
 
 cat << EOF > scriptfile.tmp
-   dms01=cnc_DMS_gas(0 ,:,:)*1e9;dms01@units="nM";
-   dms02=cnc_DMS_gas(1 ,:,:)*1e9;dms02@units="nM";
-   dms03=cnc_DMS_gas(2 ,:,:)*1e9;dms03@units="nM";
-   dms04=cnc_DMS_gas(3 ,:,:)*1e9;dms04@units="nM";
-   dms05=cnc_DMS_gas(4 ,:,:)*1e9;dms05@units="nM";
-   dms06=cnc_DMS_gas(5 ,:,:)*1e9;dms06@units="nM";
-   dms07=cnc_DMS_gas(6 ,:,:)*1e9;dms07@units="nM";
-   dms08=cnc_DMS_gas(7 ,:,:)*1e9;dms08@units="nM";
-   dms09=cnc_DMS_gas(8 ,:,:)*1e9;dms09@units="nM";
-   dms10=cnc_DMS_gas(9 ,:,:)*1e9;dms10@units="nM";
-   dms11=cnc_DMS_gas(10,:,:)*1e9;dms11@units="nM";
-   dms12=cnc_DMS_gas(11,:,:)*1e9;dms12@units="nM";
+   dms01=cnc_DMS_gas(0 ,:,:)*1e9;dms01@units="nM"; where( dms01 < 0.0 ) dms01=0.0;
+   dms02=cnc_DMS_gas(1 ,:,:)*1e9;dms02@units="nM"; where( dms02 < 0.0 ) dms02=0.0;
+   dms03=cnc_DMS_gas(2 ,:,:)*1e9;dms03@units="nM"; where( dms03 < 0.0 ) dms03=0.0;
+   dms04=cnc_DMS_gas(3 ,:,:)*1e9;dms04@units="nM"; where( dms04 < 0.0 ) dms04=0.0;
+   dms05=cnc_DMS_gas(4 ,:,:)*1e9;dms05@units="nM"; where( dms05 < 0.0 ) dms05=0.0;
+   dms06=cnc_DMS_gas(5 ,:,:)*1e9;dms06@units="nM"; where( dms06 < 0.0 ) dms06=0.0;
+   dms07=cnc_DMS_gas(6 ,:,:)*1e9;dms07@units="nM"; where( dms07 < 0.0 ) dms07=0.0;
+   dms08=cnc_DMS_gas(7 ,:,:)*1e9;dms08@units="nM"; where( dms08 < 0.0 ) dms08=0.0;
+   dms09=cnc_DMS_gas(8 ,:,:)*1e9;dms09@units="nM"; where( dms09 < 0.0 ) dms09=0.0;
+   dms10=cnc_DMS_gas(9 ,:,:)*1e9;dms10@units="nM"; where( dms10 < 0.0 ) dms10=0.0;
+   dms11=cnc_DMS_gas(10,:,:)*1e9;dms11@units="nM"; where( dms11 < 0.0 ) dms11=0.0;
+   dms12=cnc_DMS_gas(11,:,:)*1e9;dms12@units="nM"; where( dms12 < 0.0 ) dms12=0.0;
 EOF
    sed -i 's/;/;\n/g' scriptfile.tmp
    ncap2 -4 -L5 -O -v -S scriptfile.tmp ${dms_file} DMS_monthly_global.nc
+
 else
    echo "DMS_monthly_global.nc already exists!"
 fi
--- CMAQ/emis/ocean4cmaq/ocean2cmaq.sh
+++ CMAQ/emis/ocean4cmaq/ocean2cmaq.sh
@@ -4,34 +4,99 @@ export LC_NUMERIC="en_US.UTF-8"
 #Input-Data:
 start_date="2019-01-01"	#"%Y-%m-%d %H"
   end_date="2019-01-01"	#"%Y-%m-%d %H"
-
    srsInp="epsg:4326"	#Los archivos vienen en latlon.
-
 #Input files:
- GRIDDESC="GRIDDESC"
- landpoly=input/shp/land_10m_global.gpkg	   #polygons to make a landmask
-chlo_file=input/CHLO_monthly_global_2019.nc     #chlorophyl conc. files (from MODIS (MY1DMM)
- dms_file=input/DMS_monthly_global_2011.nc       #dms conc. files (DMS climatology of Lana et al. (2011))
-#----------
-#Parametros de Proyeccion y Grilla (GRIDDESC)
-Gridname="test"
-truelat1=-50;truelat2=-20;stand_lon=-65;ref_lon=-65;ref_lat=-35;
-nx=197;ny=237;nz=1;dx=20000;dy=20000;     #ncols,nrows,xcel, ycell
-xorig=-1970000;yorig=-2370000;
+ GRIDDESC="GRIDDESC"    #"GRIDDESC_USA" #
+ GRIDNAME="papila_grid" #"12US1"        #
 
-read xc yc ellipsoidh <<<$( gdaltransform -s_srs "epsg:4326" -t_srs "${srsOut}" <<< $( echo "${ref_lon} ${ref_lat}" ) )
-srsOut="+proj=lcc +lat_1=${truelat1} +lat_2=${truelat2} +lon_0=${stand_lon} +lat_0=${ref_lat} +a=6370000.0 +b=6370000.0 +units=m" 
+coastline=coastline.gpkg	        #polygons to make a landmask
+ landpoly=input/shp/land_10m_global.gpkg	#polygons to make a landmask
+chlo_nc=input/CHLO_monthly_global.nc     #chlorophyl conc. files (from MODIS (MY1DMM)
+ dms_nc=input/DMS_monthly_global.nc      #dms conc. files (DMS climatology of Lana et al. (2011))
+#-------------------------------------------------
+#(0) Get grid & proj parameters from GRIDDESC:
+read projName xorig yorig dx dy nx ny nz <<< $( sed -n "/${GRIDNAME}/{n;p;q}" "$GRIDDESC" )
+read COORDTYPE P_ALP P_BET P_GAM XCENT YCENT <<< $( sed -n "/${projName}/{n;p;q}" "$GRIDDESC" )
+#read COORDTYPE truelat1 truelat2 stand_lon ref_lon ref_lat     #(en el namelist de wrf)
+#truelat1=-50;truelat2=-20;stand_lon=-65;ref_lon=-65;ref_lat=-35;
+  if [ $COORDTYPE == 1 ]; then           #Geographic:
+   srsOut="+proj=latlong +a=6370000.0 +b=6370000.0"
+elif [ $COORDTYPE == 2 ]; then     #Lambert Conformal Conic:
+   srsOut="+proj=lcc +lat_1=$P_ALP +lat_2=$P_BET +lon_0=$P_GAM +lat_0=$YCENT +a=6370000.0 +b=6370000.0 +units=m"
+elif [ $COORDTYPE == 3 ]; then     #General Mercator
+   srsOut="+proj=merc +lat_ts=$P_ALP +lon_0=$P_GAM +a=6370000.0 +b=6370000.0"
+elif [ $COORDTYPE == 4 ]; then     #General tangent Stereografic
+   srsOut="+proj=stere +lat_0=$YCENT +lon_0=$P_GAM +lat_ts=lat_ts +a=6370000.0 +b=6370000.0 +k_0=1.0"
+elif [ $COORDTYPE == 5 ]; then     #UTM
+   echo  "proyección: 5 (Universal Transverse Mercator) no soportada en esta aplicación."; stop
+elif [ $COORDTYPE == 6 ]; then     #Polar Secant Stereographic
+   srsOut="+proj=stere +lat_0=$YCENT +lon_0=$P_GAM +lat_ts=lat_ts +a=6370000.0 +b=6370000.0 +k_0=1.0"
+elif [ $COORDTYPE == 7 ]; then     #Equatorial Mercator
+   srsOut="+proj=merc +lat_ts=$P_ALP +lon_0=$P_GAM +a=6370000.0 +b=6370000.0"
+elif [ $COORDTYPE == 8 ]; then     #Transverse Mercator
+   echo  "proyección: 8 (Transverse Mercator) no soportada en esta aplicación."; stop
+elif [ $COORDTYPE == 9 ]; then     #Lambert Azimuthal Equal-Area
+   echo  "proyección: 9 (Lambert Azimuthal Equal-Area) no soportada en esta aplicación."; stop
+else
+   echo  "codigo de proyección invalido. COORDTYPE"; stop
+fi;
+
+echo "SRS of Input  Grids: $srsInp"
+echo "SRS of Output Grids: $srsOut"
 #------------------------------------------------
 #Parametros intermedios:
-xmin=$(bc -l <<<" $xc - ($xorig)*-1 ")
-xmax=$(bc -l <<<" $xc + ($xorig)*-1 ")
-ymin=$(bc -l <<<" $yc - ($yorig)*-1 ")
-ymax=$(bc -l <<<" $yc + ($yorig)*-1 ")
+xmin=$(bc -l <<<"$xorig")
+xmax=$(bc -l <<<"$xmin+$dx*$nx ")
+ymin=$(bc -l <<<"$yorig")
+ymax=$(bc -l <<<"$ymin+$dy*$ny ")
 
 #bbox latlon: para hacer clippear el archivo de fuegos al dominio:
-read lonmin latmin ellipsoidh <<<$( gdaltransform -t_srs "epsg:4326" -s_srs "${srsOut}" <<< $( echo "${xmin} ${ymin}" ) )
-read lonmax latmax ellipsoidh <<<$( gdaltransform -t_srs "epsg:4326" -s_srs "${srsOut}" <<< $( echo "${xmax} ${ymax}" ) )
+read lonmin latmin ellipsoidh <<<$( gdaltransform  -s_srs "${srsOut}" -t_srs "${srsInp}" <<< $( echo "${xmin} ${ymin}" ) )
+read lonmax latmax ellipsoidh <<<$( gdaltransform  -s_srs "${srsOut}" -t_srs "${srsInp}" <<< $( echo "${xmax} ${ymax}" ) )
+#echo "lon lat min,  $lonmin $latmin lon lat max: $lonmax $latmax"
+#------------------------------------------------
+# SURF:
+#(1) Rasterizo las lineas de costas "muy fino" (100m o 200m), asigno 1/0 si toca o no costa. Luego regrid a grilla de modelado calculando "average".
+dx_small=300; dy_small=300
+echo "Calculating SURF.."
+if [ ! -f surf_${GRIDNAME}.tif ] 
+then
+    
+    if [ ! -f ${coastline} ]
+    then
+    	ogr2ogr -f "GPKG" ${coastline} $landpoly -nln coastline -nlt MULTILINESTRING -t_srs "${srsOut}" -clipsrc "${lonmin}" "${latmin}" "${lonmax}" "${latmax}"
+    fi
+    gdal_rasterize -q -l coastline -at -burn 1 -a_srs "${srsOut}" -tr ${dx_small} ${dy_small} -te $xmin $ymin $xmax $ymax -of GTiff ${coastline} tmp.tif
+    gdalwarp -r average -t_srs "${srsOut}" -tr ${dx} ${dy} -te $xmin $ymin $xmax $ymax tmp.tif surf_${GRIDNAME}.tif
+    gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" surf_${GRIDNAME}.tif surf_${GRIDNAME}.nc 
+    rm tmp.tif
+else
+    echo "surf.tif already exists!"
+fi
+# LAND: (same procedure than with SURF)
+echo "Calculating LAND.."
+if [ ! -f land_${GRIDNAME}.tif ] 
+then
+    ogr2ogr -f "GPKG" land.gpkg $landpoly -nln land -nlt MULTIPOLYGON -t_srs "${srsOut}" -clipsrc "${lonmin}" "${latmin}" "${lonmax}" "${latmax}"
+    gdal_rasterize -q -l land -at -burn 1 -a_srs "${srsOut}" -tr ${dx_small} ${dy_small} -te $xmin $ymin $xmax $ymax -of GTiff land.gpkg tmp.tif
+    gdalwarp -r average -t_srs "${srsOut}" -tr ${dx} ${dy} -te $xmin $ymin $xmax $ymax tmp.tif land_${GRIDNAME}.tif
+    rm tmp.tif
+else
+   echo "land.tif already exists!"
+fi
+# OPEN = 1 - LAND - SURF
+echo "Calculating OPEN.."
+if [ ! -f open_${GRIDNAME}.tif ] 
+then
+    gdal_calc.py -S surf_${GRIDNAME}.tif --S_band=1 -L land_${GRIDNAME}.tif --L_band=1 --calc="1-L-S" --outfile=open_${GRIDNAME}.tif
+    gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" open_${GRIDNAME}.tif open_${GRIDNAME}.nc 
+else
+    echo "open.tif already exists!"
+fi
+#-------------------------------------------------
+#(1) Re-gridding: Agarrar los inputs y regrillarlos (e interpolar) segun GRIDDESC.
 
+#Merge them all in a NetCDF file:
 # Crear NetCDF base:
 variables=("OPEN" "SURF" "CHLO" "DMS")
 var_list=$(printf "%-16s" ${variables[@]})
@@ -40,142 +105,73 @@ var_list=$(printf "%-16s" ${variables[@]})
 cat  > netcdf_emission_template.cdl <<EOF
 netcdf ocean_1 {
 dimensions:
-    TSTEP = 1; DATE_TIME = 2; COL = $nx; ROW = $ny; LAY = 1; VAR= ${#variables[@]};
+    TSTEP = 1; DATE-TIME = 2; COL = $nx; ROW = $ny; LAY = 1; VAR= ${#variables[@]};
 variables:
-    int TFLAG(TSTEP, VAR, DATE_TIME) ;
-    float Band1(ROW, COL);
-EOF
-printf "     float %s(TSTEP, LAY, ROW, COL);\n" ${variables[@]} >> netcdf_emission_template.cdl #   //Mis variables:
-cat >> netcdf_emission_template.cdl << EOF
+  float OPEN(TSTEP, LAY, ROW, COL);
+  	OPEN:long_name = "OPEN            ";
+  	OPEN:units = "UNKNOWN         ";
+  	OPEN:var_desc = "OPEN                                                                            " ;
+  float SURF(TSTEP, LAY, ROW, COL);
+  	SURF:long_name = "SURF            " ;
+  	SURF:units = "UNKNOWN         " ;
+  	SURF:var_desc = "SURF                                                                            " ;
+  float DMS(TSTEP, LAY, ROW, COL);
+  	DMS:long_name = "DMS             " ;
+  	DMS:units = "nM              " ;
+  	DMS:var_desc = "DMS                                                                             " ;
+  float CHLO(TSTEP, LAY, ROW, COL);
+  	CHLO:long_name = "CHLO            " ;
+  	CHLO:units = "mg m^-3         " ;
+  	CHLO:var_desc = "Chlorophyll Concentration, OCI Algorithm                                        " ;
+  int TFLAG(TSTEP, VAR, DATE-TIME) ;
+  	TFLAG:units = "<YYYYDDD,HHMMSS>" ;
+  	TFLAG:long_name = "TFLAG           " ;
+  	TFLAG:var_desc = "Timestep-valid flags:  (1) YYYYDDD or (2) HHMMSS                                " ;
+  float Band1(ROW, COL);
+   //int TFLAG(TSTEP, VAR, DATE_TIME) ;
+   //float OPEN(TSTEP, LAY, ROW, COL);
+   //float SURF(TSTEP, LAY, ROW, COL);
+   //float CHLO(TSTEP, LAY, ROW, COL);
+   //float DMS(TSTEP, LAY, ROW, COL);
 // global attributes:
  :IOAPI_VERSION = "ioapi-3.2: \$Id: init3" ; :EXEC_ID = "???????????????? " ; 
  :FTYPE = 1 ; :SDATE = -635; :STIME = 000000 ; :WDATE = 2023001 ; :WTIME = 000000 ; :CDATE = 2023001 ; :CTIME = 000000;
  :TSTEP = 0; :NTHIK = 1;    
- :NCOLS = ${nx}; :NROWS = ${ny}; :NLAYS = ${nz}; :NVARS = ${#variables[@]}; :GDTYP = 2;
- :P_ALP = -50.;:P_BET = -20. ;:P_GAM = -65.; 
- :XCENT = ${ref_lon}; :YCENT = ${ref_lat}; :XORIG = ${xorig}; :YORIG = ${yorig}; :XCELL = ${dx};  :YCELL = ${dy};  
+ :NCOLS = ${nx}; :NROWS = ${ny}; :NLAYS = ${nz}; :NVARS = ${#variables[@]}; :GDTYP = ${COORDTYPE};
+ :P_ALP = ${P_ALP};:P_BET = ${P_BET} ;:P_GAM = ${P_GAM}; 
+ :XCENT = ${XCENT}; :YCENT = ${YCENT}; :XORIG = ${xorig}; :YORIG = ${yorig}; :XCELL = ${dx};  :YCELL = ${dy};  
  :VGTYP = -9999 ;:VGTOP = 5000.f ;:VGLVLS = 1.f, 0.9938147f;    
- :GDNAM = "${GridName}";:UPNAM = "OUTCM3IO" ;   :VAR-LIST = "${var_list}";      
- :FILEDESC = "Merged emissions output file from Mrggrid" ; :HISTORY = "" ;
+ :GDNAM = "${GRIDNAME}";:UPNAM = "CONVERT" ;   :VAR-LIST = "${var_list}";      
+ :FILEDESC = "OCEAN file" ; :HISTORY = "" ;
 }
 EOF
-#------------------------------------------------
-# SURF:
-#(1) Rasterizo las lineas de costas "muy fino" (100m o 200m), asigno 1/0 si toca o no costa. Luego regrid a grilla de modelado calculando "average".
-dx_small=200; dy_small=200
-echo "Calculating SURF.."
-if [ ! -f SURF.tif ] 
-then
-    ogr2ogr -f "GPKG" coastline.gpkg $landpoly -nln coast -nlt MULTILINESTRING -t_srs "${srsOut}" -clipsrc $lonmin $latmin $lonmax $latmax #-explodecollections 
-    gdal_rasterize -q -l coast -at -burn 1 -a_srs "${srsOut}" -tr ${dx_small} ${dy_small} -te $xmin $ymin $xmax $ymax -of GTiff coastline.gpkg tmp.tif
-    gdalwarp -r average -t_srs "${srsOut}" -tr ${dx} ${dy} -te $xmin $ymin $xmax $ymax tmp.tif SURF.tif 
-    rm tmp.tif
-else
-    echo "SURF.tif already exists!"
-fi
-# LAND: (same procedure than with SURF)
-echo "Calculating LAND.."
-if [ ! -f LAND.tif ] 
-then
-    ogr2ogr -f "GPKG" land.gpkg $landpoly -nln land -nlt MULTIPOLYGON -t_srs "${srsOut}" -clipsrc $lonmin $latmin $lonmax $latmax
-    gdal_rasterize -q -l land -at -burn 1 -a_srs "${srsOut}" -tr ${dx_small} ${dy_small} -te $xmin $ymin $xmax $ymax -of GTiff land.gpkg tmp.tif
-    gdalwarp -r average -t_srs "${srsOut}" -tr ${dx} ${dy} -te $xmin $ymin $xmax $ymax tmp.tif LAND.tif 
-    rm tmp.tif
-else
-   echo "LAND.tif already exists!"
-fi
-# OPEN = 1 - LAND - SURF
-echo "Calculating OPEN.."
-if [ ! -f OPEN.tif ] 
-then
-    gdal_calc.py -S SURF.tif --S_band=1 -L LAND.tif --L_band=1 --outfile=OPEN.tif --calc="1-L-S" 
-else
-    echo "OPEN.tif already exists!"
-fi
-#--------------------------------
-#Merge them all in a NetCDF file:
-for mm in $(seq --format="%02.0f" 0 12) 
-do
-	#Creo NetCDF destino:
-	file_out=fire_emis_${YYYY}${DDD}_${HH}:00:00_d01.nc
-	ncgen -o $file_out netcdf_emission_template.cdl
 
-    gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" ${file} tmp0.nc 
-    ncks -4 -A -v Band1 tmp0.nc tmp.nc
-    ncrename -v Band1,chlo${mm} tmp.nc
-    #ncatted -h -O -a esri_pe_string,laiv${mm},d,c,"" tmp.nc
-    ncatted -h -O -a units,chlo${mm},o,c,"mg m^-3" tmp.nc
-    ncatted -h -O -a long_name,chlo${mm},o,c,"CHLO" tmp.nc
-    ncatted -h -O -a standard_name,chlo${mm},o,c,"mass_concentration_of_chlorophyll_in_sea_water" tmp.nc
-    ncatted -h -O -a var_desc,chlo${mm},o,c,"Chlorophyll Concentration, from MY1DMM MODIS product." tmp.nc
-    ncatted -h -O -a missing_value,chlo${mm},o,s,"99999" tmp.nc
+for mm in $(seq --format="%02.0f" 1 12) 
+do
+   #Creo NetCDF destino:
+   file_out=OCEAN_${GRIDNAME}_${mm}.nc 
+   echo "Creating: ${file_out} .."
+   ncgen -o $file_out netcdf_emission_template.cdl
+   #OPEN
+   ncks -4 -A -h -v Band1 open_${GRIDNAME}.nc ${file_out}
+   ncap2 -A -h -s "OPEN(0,0,:,:)=float(Band1(:,:))" $file_out $file_out
+   #SURF
+   ncks -4 -A -h -v Band1 surf_${GRIDNAME}.nc ${file_out}
+   ncap2 -A -h -s "SURF(0,0,:,:)=float(Band1(:,:))" $file_out $file_out
+   ##CHLO
+   gdalwarp -dstnodata 99999 -q -overwrite -s_srs "$srsInp" -t_srs "$srsOut" -te $xmin $ymin $xmax $ymax -tr $dx $dy -r average -f "NetCDF" NETCDF:${chlo_nc}:chlo${mm} tmp0.nc
+   ncks -4 -A -h -v Band1 tmp0.nc ${file_out}
+   ncap2 -A -h -s "CHLO(0,0,:,:)=float(Band1); where (CHLO == 99999 ) CHLO = 0" $file_out $file_out
+   ncatted -h -O -a missing_value,CHLO,o,f,99999 ${file_out}
+   rm tmp0.nc
+   ##DMS
+   gdalwarp -dstnodata -999 -q -overwrite -s_srs "$srsInp" -t_srs "$srsOut" -te $xmin $ymin $xmax $ymax -tr $dx $dy -r average -f "NetCDF" NETCDF:${dms_nc}:dms${mm} tmp0.nc
+   ncks -4 -A -h -v Band1 tmp0.nc ${file_out}
+   ncap2 -A -h -s "DMS(0,0,:,:)=float(Band1); where ( DMS < 0 ) DMS = 0" $file_out $file_out
+   #TFLAG
+   ncap2 -O -h -s "TFLAG(:,:,:) = 0"  $file_out $file_out
+
+   ncks -O -x -h -v Band1,mercator ${file_out} ${file_out}
+   ncatted -h -O -a missing_value,global,o,s,"" ${file_out}
 done
-rm tmp0.nc
-mv tmp.nc OCEAN_MASK.nc
-##---------------------------------------
-#mkdir finn_data		#carpeta de datos descargados de FINN.
-#thisYear=$( date +"%Y")
-#
-#day=$start_date_s
-#while [ $day -le $end_date_s ]
-#do
-#	YYYY=$(date -d @$day +"%Y")   #año
-#	  MM=$(date -d @$day +"%m")   #mes
-#	 DDD=$(date -d @$day +"%j")   #dia juliano
-#	  HH=$(date -d @$day +"%H")   #hora
-#	echo "Dia: $YYYY-$DDD (mes: $MM)"
-#
-#	#=================
-#	#Transformo coordenadas + me quedo con los puntos dentro del dominio.
-#	cat $finnFile | sed 's/D\([-+]\)/e\1/g;s/,/;/g' | awk -F";" 'NR==1{print $0";wkt"} NR>=2{print $0"POINT("$5*1.0,$4*1.0")"}' > tmp.csv
-#	ogr2ogr -f CSV tmp_ll_clipped.csv tmp.csv -clipsrc $lonmin $latmin $lonmax $latmax -lco GEOMETRY="AS_WKT" -lco SEPARATOR="SEMICOLON"
-#	ogr2ogr -f CSV tmp_xy.csv tmp_ll_clipped.csv -s_srs "$srsInp" -t_srs "$srsOut" -lco GEOMETRY="AS_WKT" -lco SEPARATOR="SEMICOLON"
-#	sed -i 's/"//g;s/  */ /g;' tmp_xy.csv
-#	#=================
-#	#Un archivo por hora:
-#	
-#	for HH in $(seq --format="%02.0f" 0 23) 
-#	do
-#		echo "   HORA: $HH"
-#		i="$(printf "%d" ${HH})";
-#	
-#		#Peso horario:
-#		wgt_hh=${ciclo_diurno[i]}
-#
-#		#Creo NetCDF destino:
-#		file_out=fire_emis_${YYYY}${DDD}_${HH}:00:00_d01.nc
-#		ncgen -o $file_out netcdf_emission_template.cdl
-#	
-#		#---------------------------------------
-#		for j in ${!polluts[@]}
-#		do
-#			pollut=${polluts[$j]}
-#			echo "      pollut: $pollut"
-#			#---------------------------------------
-#			#Filtro columnas wkt y pollut, tambien convierto emis a mole/s ó g/s (aplicando ciclo diurno).
-#			awk -F";" -v pollut=${pollut} -v wgt_hh=${wgt_hh} '
-#			NR==1{
-#			for(i=1;i<=NF;i++){if($i==pollut){pol=i};};
-#				print "wkt;emis";
-#				if ( pollut == "OC" || pollut =="BC" || pollut=="PM25" || pollut == "PM10" ){
-#					k=1/3600000*wgt_hh;	#kg/day -> g/s	
-#				}else {
-#					k=1/3600.0 *wgt_hh;	#mole/day -> mole/s
-#				};
-#			} 
-#			NR>1{ printf("%s;%.5e\n", $1, $pol*k); }' tmp_xy.csv > tmp_pollut.csv
-#			#---------------------------------------
-#			##ASCII to netcdf
-#			#gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" -ot Float32 tmp_pollut.csv tmp.nc
-#			gdal_rasterize -q -a emis -add -a_srs "${srsOut}" -tr $dx $dy -te $xmin $ymin $xmax $ymax -of GTiff tmp_pollut.csv tmp.tif
-#			gdal_translate -q -a_srs "$srsOut" -ot Float32 -of netCDF -co "FORMAT=NC4" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" tmp.tif tmp.nc #-co "WRITE_LONLAT=YES" -co "WRITE_BOTTOMUP=NO"
-#	
-#			ncks  -h -A -V -v Band1 tmp.nc -o $file_out
-#			ncap2 -h -A -C -s "${pollut}(0,0,:,:) = Band1(:,:); TFLAG(0,$j,0) = ${YYYY}${DDD}; TFLAG(0,$j,1) =  ${HH}0000;" $file_out 
-#
-#		done;
-#		
-#	done;
-#	day=$(($day + 86400 )) # Incrementar day un dia
-#done;
-#
+rm tmp0.nc 
