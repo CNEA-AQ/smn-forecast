2d2261318719e4d8e779ad8d3d81df2c06538658
ramespada <espada@agro.uba.ar>
2024-06-14 23:47:47 -0300

    correcciones emis y wrf2griddesc

--- forecast/ope/prep-emis.sh
+++ forecast/ope/prep-emis.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-today=`date +'%Y-%m-%d'`
-yesterday=`date -d "yesterday" +'%Y-%m-%d'`
-tomorrow=`date -d "tomorrow" +'%Y-%m-%d'`
+today="2024-06-11" #`date +'%Y-%m-%d'`
+yesterday=`date -d "${today} - 1 day" +'%Y-%m-%d'`
+tomorrow=`date -d "${today}  + 1 day" +'%Y-%m-%d'`
+
 #Prep. Emissions:
 echo "* * * * * * * * * * * * "
 echo "* Prep emission files   "
@@ -10,7 +11,8 @@ echo "* Prep emission files   "
 #============================================================
 # Silam:
 basedir=${HOME}/github/CNEA-AQ/smn-forecast/forecast
-wrf_dir=${basedir}/ope/wrf
+#wrf_dir=${basedir}/ope/wrf
+wrf_dir=${HOME}/forecast/ope/wrf
 emis_dir=${HOME}/data/silam/emis
 
 if [ ! -d slm ]; then mkdir slm; mkdir slm/emis; fi
@@ -22,7 +24,7 @@ echo -e "Processing \e[35m static emission files\e[0m (CAMs v5.3, dust, soil-NO,
 #Static emision files:
  ln -s ${emis_dir}/CAMS_GLOB_v53_ARG_CB5v2_as_2022 .   # Anthro (CAMS  v5.3)
  ln -s ${emis_dir}/dust-simple                     .   # dust emis             
- ln -s ${emis_dir}/soil-NO                         .   # soil NO            
+ ln -s ${emis_dir}/soil_no                         .   # soil NO            
  ln -s ${emis_dir}/sslt                            .   # sea salt and DMS   
 #ln -s ${emis_dia}/lightning                       .   # thunders           
 #ln -s ${emis_dia}/aircraft                        .   # airplanes
@@ -32,13 +34,12 @@ echo -e "Processing \e[35m static emission files\e[0m (CAMs v5.3, dust, soil-NO,
 echo -e "Processing \e[32m BVOCs (w/MEGAN) v3.3\e[0m"
  cp -r ${emis_dir}/megan3.3    .   # sea salt and DMS   
  cd megan3.3
-
+ #edito namelist:
  sed -i "s/start_date.*/start_date=\"${today} 00:00:00\"/" megan_namelist
  sed -i "s/end_date.*/end_date=\"${tomorrow} 00:00:00\"/" megan_namelist
  sed -i "s@met_files.*@met_files=\"${wrf_dir}/wrfout_d01_<date>_00:00:00\"@" megan_namelist
-
- ./megan_v3.3.exe < megan_namelist
- if [ ! -f emis_bio_d01* ] ; then ./megan_v3.3.exe < megan_namelist; fi
+ #ejecuto:
+ ./megan_v3.3.exe < megan_namelist                                        # prep-megan ya deberia estár corrido en emis_dir
  
 cd ..
 #----------------------
@@ -46,20 +47,21 @@ cd ..
 echo -e "Processing \e[31m Fires (w/FINN v2.5)\e[0m "
  cp -r ${emis_dir}/finn2.5  .   # sea salt and DMS   
  cd finn2.5
- 
- sed -i "s/start_date=.*/start_date=\"${yesterday}\"/" fires_namelist
- sed -i "s/end_date=.*/end_date=\"${yesterday}\"/" fires_namelist
+ #edito namelist
+ sed -i "s/start_date=.*/start_date=\"${yesterday}\"/" fires_namelist     #Acá estoy haciendo "trampa" por que uso los fuegos de ayer!
+ sed -i "s/end_date=.*/end_date=\"${yesterday}\"/"     fires_namelist     #todavia no hay disponible un pronóstico de fuegos..
+ #ejecuto:
  ./finn2silam.exe < fires_namelist
 
  newStart=`date +'seconds since %Y-%m-%d 00:00:00 UTC'`
  ncatted -h -O -a units,time,m,c,"${newStart}" emis_fires*
+
 cd ..
 #============================================================
 # CMAQ
 #
 # newTitle=" OUTPUT FROM WRF V4    MODEL"
 # ncatted -h -O -a title,global,m,c,"${newTitle}" ${wrf_dir}/wrfout_d01_*
-
 
 #============================================================
 
--- forecast/ope/wrf2griddesc.sh
+++ forecast/ope/wrf2griddesc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export LC_NUMERIC="en_US.UTF-8"
 
-#GRIDDESC FILE:
-#  COORD-NAME
-#    COORDTYPE, P_ALP, P_BET, P_GAM, XCENT, YCENT
-#  GRID-NAME
-#    COORD-NAME, XORIG, YORIG, XCELL, YCELL, NCOLS, NROWS, NTHIK
-
+#GRIDDESC FILE:-----------------------------------------------------
+#  COORD-NAME                                                      |   
+#    COORDTYPE, P_ALP, P_BET, P_GAM, XCENT, YCENT                  |  
+#  GRID-NAME                                                       |  
+#    COORD-NAME, XORIG, YORIG, XCELL, YCELL, NCOLS, NROWS, NTHIK   |
+#-------------------------------------------------------------------
     wrfout_file=$1
 
     if [[ ! -f "$wrfout_file" ]]; then
@@ -15,22 +15,20 @@ export LC_NUMERIC="en_US.UTF-8"
     fi
 
     # Extract grid information from the WRFOUT file
-    map_proj=$(ncdump -h "$wrfout_file" | grep 'MAP_PROJ ='                    | awk '{printf("%s"  , $3)}')
+    ncdump -h "$wrfout_file"  > tmp_ncdump_-h
+    map_proj=$(tail -n 200 tmp_ncdump_-h | grep 'MAP_PROJ ='                    | awk '{printf("%s"  , $3)}')
+    nx=$(      tail -n 200 tmp_ncdump_-h | grep 'WEST-EAST_GRID_DIMENSION ='    | awk '{print $3}')
+    ny=$(      tail -n 200 tmp_ncdump_-h | grep 'SOUTH-NORTH_GRID_DIMENSION ='  | awk '{print $3}')
+    dx=$(      tail -n 200 tmp_ncdump_-h | grep 'DX ='           | sed 's/f//g' | awk '{printf("%8.3f",$3)}')
+    dy=$(      tail -n 200 tmp_ncdump_-h | grep 'DY ='           | sed 's/f//g' | awk '{printf("%8.3f",$3)}')
+    clat=$(    tail -n 200 tmp_ncdump_-h | grep 'CEN_LAT ='      | sed 's/f//g' | awk '{printf("%8.5f",$3)}')
+    clon=$(    tail -n 200 tmp_ncdump_-h | grep 'CEN_LON ='      | sed 's/f//g' | awk '{printf("%8.5f",$3)}')
+     ref_lat=$(tail -n 200 tmp_ncdump_-h | grep 'MOAD_CEN_LAT =' | sed 's/f//g' | awk '{printf("%8.3f",$3)}')
+     ref_lon=$(tail -n 200 tmp_ncdump_-h | grep 'STAND_LON ='    | sed 's/f//g' | awk '{printf("%8.3f",$3)}')
+    truelat1=$(tail -n 200 tmp_ncdump_-h | grep 'TRUELAT1 ='     | sed 's/f//g' | awk '{printf("%8.5f",$3)}')
+    truelat2=$(tail -n 200 tmp_ncdump_-h | grep 'TRUELAT2 ='     | sed 's/f//g' | awk '{printf("%8.5f",$3)}')
+    rm tmp_ncdump_-h
 
-    nx=$(      ncdump -h "$wrfout_file" | grep 'WEST-EAST_GRID_DIMENSION ='    | sed 's/f//g' | awk '{print $3}')
-    ny=$(      ncdump -h "$wrfout_file" | grep 'SOUTH-NORTH_GRID_DIMENSION ='  | sed 's/f//g' | awk '{print $3}')
-
-
-    dx=$(      ncdump -h "$wrfout_file" | grep 'DX ='           | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    dy=$(      ncdump -h "$wrfout_file" | grep 'DY ='           | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    ref_lat=$( ncdump -h "$wrfout_file" | grep 'MOAD_CEN_LAT =' | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    ref_lon=$( ncdump -h "$wrfout_file" | grep 'STAND_LON ='    | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    truelat1=$(ncdump -h "$wrfout_file" | grep 'TRUELAT1 ='     | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    truelat2=$(ncdump -h "$wrfout_file" | grep 'TRUELAT2 ='     | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    clat=$(    ncdump -h "$wrfout_file" | grep 'CEN_LAT ='      | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-    clon=$(    ncdump -h "$wrfout_file" | grep 'CEN_LON ='      | sed 's/f//g' | awk '{printf("%.3f", $3)}')
-  
-#echo $dx,$dy, $ref_lat, $ref_lon, $truelat1 $truelat2 $clat $clon
   #map_proj =
   #1: Lambert Conformal
   #2: Polar Stereographic
@@ -40,34 +38,36 @@ export LC_NUMERIC="en_US.UTF-8"
   #if (( $map_proj == 1 )) #'lambert' ) 
   #then
     coordtype=2
-    alp=$truelat1; bet=$truelat2; gam=$ref_lon
-    proj_str="+proj=lcc +a=6370000.0 +b=6370000.0 +lat_1=$truelat1 +lat_2=$truelat2 +lon_0=$ref_lon"
-#echo $proj_str
+    alp=${truelat1}
+    bet=${truelat2}
+    gam=${ref_lon}
+    proj_str="+proj=lcc +lat_1=${truelat1} +lat_2=${truelat2} +lat_0=${ref_lat} +lon_0=${ref_lon} +unit=m" #+a=6370000.0 +b=6370000.0 
+    proj_str=${proj_str//= /=}
   #elif (( $map_proj == 2 )) 
   #   coordtype=4
   #   proj_str=""
   #elif (( $map_proj == 3 ))  
   #   coordtype=3
-  #   proj_str="+proj=merc +a=6370000.0 +b=6370000.0 +lon_0=${ref_lon} +lat_ts="
+  #   proj_str="+proj=merc +a=6370000.0 +b=6370000.0 +lon_0=${ref_lon} +lat_ts=${ref_lat}"
   #fi
-
+echo "proj:" $proj_str
   read xc yc ellipsoidh <<<$( gdaltransform -s_srs epsg:4326 -t_srs "${proj_str}" <<< $( echo "${clon} ${clat}" ) )
-  xmin=$(bc -l <<<"$xc-$dx*$nx*1.0")
+  xmin=$(bc -l <<<"$xc-$dx*$nx*0.5")
   ymin=$(bc -l <<<"$yc-$dy*$ny*0.5")
-#echo "xc yc "$xc, $yc
-#echo "xmin, ymin" $xmin, $ymin
+  echo "xc yc "$xc, $yc
+  echo "xmin, ymin" $xmin, $ymin
   
   grid_name=TEST_GRID
   coord_name=TEST_COORD
   # Format and write the GRIDDESC file
- echo "'${coord_name}'                                  "
- echo "${coordtype} $alp $bet $gam $clon $clat        "
- echo "'${grid_name}'                                   "
- echo "'${coord_name}'  $xmin $ymin $dx $dy $nx $ny $nz "
+#@ echo "'${coord_name}'                                  "
+#@ echo "${coordtype} $alp $bet $gam $clon $clat        "
+#@ echo "'${grid_name}'                                   "
+#@ echo "'${coord_name}' $xmin $ymin $dx $dy $nx $ny $nz "
 
 printf "%-12s \n" ${coord_name}
-printf "%-6d %.4f %9.6f %9.6f %9.6f %9.5f\n" ${coordtype} $alp $bet $gam $clon $clat        
+printf "%-8d %9.5f %9.5f %9.5f %9.5f %10.7f \n" ${coordtype} ${alp} ${bet} ${gam} ${clon} ${clat};
 printf "%-12s \n" ${grid_name}
-printf "%.2f %.2f %.1f %.1f %5d %5d %5d\n" $xmin $ymin $dx $dy $nx $ny 1
+printf "%-12s %.1f %.1f %.2f %.2f %5d %5d %5d\n" ${coord_name} $xmin $ymin $dx $dy $nx $ny 1
 
 
